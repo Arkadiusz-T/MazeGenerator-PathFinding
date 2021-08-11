4cb76996d5d9047535717412b80b348878489677
Arkadiusz-design <arkadiusz.tokarski.789@gmail.com>
2021-08-11 22:40:46 +0200

    updated coments and now random mouse algorithm is working properly

--- PathFinder.py
+++ PathFinder.py
@@ -3,42 +3,49 @@ from random import randint
 
 class PathFinder:
     @staticmethod
-    def random_mouse_algorithm(maze, size=[10, 10]):
-        solution = maze.structure
-        current_location = maze.entries[0]
-        solution_not_ended = True
-        end_location = maze.entries[1]
+    def random_mouse_algorithm(maze):
+        # declare variables
+        size = maze.size
+        solution = maze.structure           # placeholder for solution
+        current_location = maze.entries[0]  # start search from the entry
+        solution_not_ended = True           # solution it yet to find
+        end_location = maze.entries[1]      # set exit coordinates
         direction = int()
-        can_make_a_step = True
+        can_make_a_step = True              # there is no wall in front of the entry
 
         # find first direction to go into the maze
-        if maze.entries[0][0] == 0:
+        if current_location[0] == 0:
             direction = 2
 
-        elif maze.entries[0][1] == 0:
+        elif current_location[1] == 0:
             direction = 1
 
-        elif maze.entries[0][0] == size[0] * 2:
+        elif current_location[0] == size[0] * 2:
             direction = 0
 
-        elif maze.entries[0][1] == size[1] * 2:
+        elif current_location[1] == size[1] * 2:
             direction = 3
 
         while solution_not_ended:
+            # continue until algorithm reach the exit
             while can_make_a_step:
+                #
                 potential_next_location = list()
                 try:
+                    # check if on junction
                     potential_next_locations = list()
                     potential_next_locations.append(solution[current_location[0] - 1][current_location[1]])
                     potential_next_locations.append(solution[current_location[0]][current_location[1] + 1])
                     potential_next_locations.append(solution[current_location[0] + 1][current_location[1]])
                     potential_next_locations.append(solution[current_location[0]][current_location[1] - 1])
                     if potential_next_locations.count('#') != 2:
-                        # if on junction try to change direction
+                        # if on junction then try to change direction
                         direction = randint(0, 3)
                 except IndexError:
-                    #  near end of the maze
+                    # first or last iteration of the algorithm, there is no wall to prevent it from moving
+                    # there is no need to check fot a junction
                     pass
+
                 if direction == 0:
                     # go up
                     potential_next_location = [current_location[0] - 1, current_location[1]]
@@ -55,23 +62,14 @@ class PathFinder:
                     # go left
                     potential_next_location = [current_location[0], current_location[1] - 1]
 
-                try:
-                    if potential_next_location != maze.entries[0]:
-                        if solution[potential_next_location[0]][potential_next_location[1]] != '#':
-                            solution[current_location[0]][current_location[1]] = '.'
-                            current_location = potential_next_location
-
-                except IndexError:
+                if potential_next_location != maze.entries[0] \
+                   and solution[potential_next_location[0]][potential_next_location[1]] != '#':
                     solution[current_location[0]][current_location[1]] = '.'
-                    maze.solution = solution
-                    can_make_a_step = False
-                    solution_not_ended = False
-                    print(current_location, potential_next_location)
-
+                    current_location = potential_next_location
                 else:
                     can_make_a_step = False
 
-                if not current_location != end_location:
+                if current_location == end_location:
                     can_make_a_step = False
                     solution_not_ended = False
                 maze.solution = solution
